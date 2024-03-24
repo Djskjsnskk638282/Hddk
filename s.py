1eda7aba024c2822e8973bc1cb6c81a79b102d48
Djskjsnskk638282 <163914866+Djskjsnskk638282@users.noreply.github.com>
2024-03-25 03:53:24 +0700

    Update s.py

--- s.py
+++ s.py
@@ -23,6 +23,8 @@ from selenium import webdriver
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
+chrome_options.add_argument('--headless')
+
 
 # set the target URL
 url = "https://suhuwaktogel.land/"
